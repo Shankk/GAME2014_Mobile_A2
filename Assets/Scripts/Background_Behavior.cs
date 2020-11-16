373c7c7d43987298fbd30a142839b4d3761b0846
Brian <46753834+Shankk@users.noreply.github.com>
2020-11-15 23:32:42 -0500

    Initial commit

---
+++ Assets/Scripts/Background_Behavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Behavior : MonoBehaviour
+{
+    public int Speed;
+    public int HorizontalBoundary;
+    public int HorizontalStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBackground();
+        ResetBackground();
+    }
+    void MoveBackground()
+    {
+        transform.position -= new Vector3(Speed * Time.deltaTime, 0, 0);
+    }
+
+    void ResetBackground()
+    {
+        if(transform.position.x < HorizontalBoundary)
+        {
+            transform.position = new Vector3(HorizontalStart, 0, 0);
+        }
+    }
+}
